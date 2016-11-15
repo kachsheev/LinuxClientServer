010bd4e14fec23a0d173a91e61691de174bbf30b
Anton Kashcheev <antonkachsheev@gmail.com>
2016-11-15 21:02:02 +0300

    Added protocol types and socket initialization.

--- Common/Connection.cpp
+++ Common/Connection.cpp
@@ -1,1 +1,41 @@
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include "Connection.hpp"
+
+Connection::Connection() : Connection(nullptr, nullptr)
+{
+}
+
+Connection::Connection(const char *strProtocol, const char *strPort):
+		sock(0),
+		protocol(Protocol::UNKNOWN)
+{
+	using std::strcmp;
+	using std::atoi;
+
+	memset(&sockAddr, 0, sizeof(sockAddr));
+	if (strPort != nullptr && strPort != nullptr)
+	{
+		if (strcmp("TCP", strProtocol))
+		{
+			protocol = Protocol::TCP;
+		}
+		else if (strcmp("TCP", strProtocol))
+		{
+			protocol = Protocol::UDP;
+		}
+		sock = socket(AF_INET, SOCK_STREAM, static_cast<int>(protocol));
+		sockAddr.sin_port = static_cast<in_port_t>(atoi(strPort));
+	}
+}
+
+Connection::~Connection()
+{
+	if (sock > 0)
+	{
+		close(sock);
+	}
+}
--- Common/Connection.hpp
+++ Common/Connection.hpp
@@ -1,12 +1,43 @@
-#ifndef CONNECTION_HPP
-#define CONNECTION_HPP
+#ifndef COMMON_CONNECTION_HPP
+#define COMMON_CONNECTION_HPP
 
-#include <sys/socket.h>
+#include <netinet/in.h>
 
 class Connection
 {
 public:
+	enum class Protocol: int
+	{
+		UNKNOWN = IPPROTO_IP,
+		TCP = IPPROTO_TCP,
+		UDP = IPPROTO_UDP
+	};
+
+	Connection();
+	Connection(const char *strProtocol, const char *strPort);
+	virtual ~Connection();
+
+	Protocol getProtocol()
+	{
+		return protocol;
+	}
+
+	int getPort()
+	{
+		return sockAddr.sin_port;
+	}
+
+	int getSocket()
+	{
+		return sock;
+	}
+
+	virtual bool connect() = 0;
+
 private:
+	int sock;
+	sockaddr_in sockAddr;
+	Protocol protocol;
 };
 
-#endif // CONNECTION_HPP
+#endif // COMMON_CONNECTION_HPP
